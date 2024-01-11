a89836d487fcc18c40ae25d563ad6b096bee8278
Minhajulislam0 <mdmishu98730@gmail.com>
2024-01-11 23:23:37 +0600

    Starter

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import RandomColor from "./components/RandomColor";
+
+export default function App() {
+  return (
+    <div>
+      <RandomColor />
+    </div>
+  );
+}
---
+++ src/components/RandomColor.jsx
@@ -0,0 +1,11 @@
+import "./RandomColor.css";
+
+export default function RandomColor() {
+  return (
+    <div className="container">
+      <button>Generate HEX Color</button>
+      <button>Generate RGB Color</button>
+      <button>Generate Random Color</button>
+    </div>
+  );
+}
